0a2444f885e4267a5b1c34bd0d5a575057dcdd4a
nouali islem <islem.nouali@isgb.ucar.tn>
2025-06-30 11:45:54 +0100

    aaa

--- render-build.sh
+++ render-build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# Download and extract OpenJDK 17
+# Download OpenJDK 17 from Eclipse Temurin
 mkdir -p ~/.java
-curl -L -o openjdk.tar.gz https://download.bell-sw.com/java/17.0.11+9/bellsoft-jdk17.0.11+9-linux-amd64.tar.gz
+
+curl -L -o openjdk.tar.gz https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.11+9/OpenJDK17U-jdk_x64_linux_hotspot_17.0.11_9.tar.gz
+
+# Extract it
 tar -xzf openjdk.tar.gz -C ~/.java --strip-components=1
 
-# Set JAVA_HOME and update PATH
+# Set JAVA_HOME and PATH
 export JAVA_HOME="$HOME/.java"
 export PATH="$JAVA_HOME/bin:$PATH"
 
-# Confirm it works
+# Confirm installation
 java -version

ac251de5b8a3a8bde4a6111b1313a3aa37af8f61
nouali islem <islem.nouali@isgb.ucar.tn>
2025-07-01 00:10:21 +0100

    Update settings.py

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f&-lp9mdr1+f57+e-%$msolu3nnk(fvsr66ac^+tlzs=^mo-j_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['resultats-stats-viewer-production.up.railway.app']
 
 
 # Application definition
